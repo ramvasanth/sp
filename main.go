063cca530df1334c1f1794166a6f4135af591e06
Ram <rama.moorthy@razer.com>
2019-05-27 11:42:30 +0800

    add main file for running service

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ramvasanth/sp/config"
+	"github.com/ramvasanth/sp/controllers"
+	"github.com/ramvasanth/sp/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	loadDeps()
+	log.Println("dependecies are loaded")
+	gin.SetMode(gin.ReleaseMode)
+	g := gin.New()
+	g.Use(gin.Recovery())
+	controllers.LoadRoutes(g)
+
+	listenAddress := fmt.Sprintf("0.0.0.0:%d", 3000)
+	log.Println("service is running at ", listenAddress)
+	g.Run(listenAddress)
+}
+
+func loadDeps() {
+	cfg, err := config.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config.Initialize(cfg)
+
+	db, err := models.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	models.Set(db)
+}
